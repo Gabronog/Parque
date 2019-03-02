e300e07bab4072ed46c4f3013635d1dc708d2423
gabronog <gnogueral1@alumno.uned.es>
2019-03-02 21:32:58 +0100

    Serializacion de Atracciones
    Signed-off-by: gabronog <gnogueral1@alumno.uned.es>

---
+++ src/Atracciones/GestorAtracciones.java
@@ -0,0 +1,47 @@
+package Atracciones;
+
+import java.io.*;
+import java.util.LinkedList;
+
+public abstract class GestorAtracciones {
+
+    private static LinkedList<Atraccion> Atracciones = new LinkedList<>();
+
+    public static void NuevaAtraccion(Atraccion Atraccion) {
+        Atracciones.add(Atraccion);
+    }
+
+    public static Atraccion ObtenerDatos(int numeroAtraccion) {
+        return Atracciones.get(numeroAtraccion);
+    }
+
+    public static void Guardar() {
+        try {
+            FileOutputStream fos = new FileOutputStream("list.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(Atracciones);
+            oos.close();
+            fos.close();
+            System.out.println("Datos guardados correctamente!");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static void Cargar() {
+        try {
+            FileInputStream fis = new FileInputStream("list.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Atracciones.addAll((LinkedList) ois.readObject());
+            ois.close();
+            fis.close();
+            System.out.println("Datos cargados correctamente");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println("Se produjo un error al cargar los datos");
+        } catch (ClassNotFoundException c) {
+            System.out.println("Clase no encontrada.");
+            c.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import Atracciones.GestorAtracciones;
 import Persona.GestorUsuarios;
 
 public class Main {
     public static void main(String[] args) {
+        CargarDatos(); //Carga los datos serializados previamente de atracciones y clientes
+
+        GuardarDatos(); //Guarda los datos de las atracciones y de los clientes
+    }
+
+    private static void CargarDatos() {
         GestorUsuarios.Cargar();
+        GestorAtracciones.Cargar();
+    }
+
+    private static void GuardarDatos() {
+        GestorAtracciones.Guardar();
+        GestorUsuarios.Guardar();
     }
 }
